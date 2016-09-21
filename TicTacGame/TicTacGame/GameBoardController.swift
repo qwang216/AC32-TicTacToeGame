b0179638b1e939914752eaca0f34f32f338a747c
Jason Wang <qwang216@gmail.com>
2016-09-20 23:29:50 -0400

    clean up

--- TicTacGame/TicTacGame/GameBoardController.swift
+++ TicTacGame/TicTacGame/GameBoardController.swift
@@ -13,8 +13,8 @@ class GameBoardController: UIViewController {
     var isPlayerOne = true
 
     let game = TicTacToe()
-    let resetGameTitleName = "Player1: Red  |  Player2: Green"
 
+    let resetGameTitleName = "Player1: Red  |  Player2: Green"
     var gameTitleLabel: UILabel = {
         let label = UILabel()
         label.textColor = .blackColor()
@@ -25,21 +25,27 @@ class GameBoardController: UIViewController {
 
     var containerView: UIView = {
         let view = UIView()
-        view.backgroundColor = .lightGrayColor()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
 
+    let resetButtonName = "Reset Game"
+    let resetButton: UIButton = {
+        let button = UIButton(type: .System)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .whiteColor()
         setupGameBoard()
     }
 
+    // MARK: - SetupGame
     func setupGameBoard() {
-        // setup nav bar title
-        navigationItem.title = "TicTacToe Game"
-
         setupContainerView()
         setupGameTitleLabel()
         setupResetButton()
@@ -54,6 +60,8 @@ class GameBoardController: UIViewController {
 
     }
 
+
+    // MARK: - Constrains
     func setupContainerView() {
         view.addSubview(containerView)
         containerView.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
@@ -72,13 +80,6 @@ class GameBoardController: UIViewController {
         gameTitleLabel.rightAnchor.constraintEqualToAnchor(view.rightAnchor, constant: -20).active = true
     }
 
-    let resetButtonName = "Reset Game"
-    let resetButton: UIButton = {
-        let button = UIButton(type: .System)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
     func setupResetButton() {
         containerView.addSubview(resetButton)
         resetButton.setTitle(resetButtonName, forState: .Normal)
@@ -90,14 +91,6 @@ class GameBoardController: UIViewController {
         resetButton.centerXAnchor.constraintEqualToAnchor(containerView.centerXAnchor).active = true
         resetButton.heightAnchor.constraintEqualToConstant(50).active = true
         resetButton.widthAnchor.constraintEqualToConstant(100).active = true
-    }
-
-    func handleResetButton(resetButton: UIButton) {
-        // reset all the colors... let's make a function that reset all the button colors to blue except the resetbutton itself
-        isPlayerOne = true
-        resetAllButtons(shouldResetButtonColor: true, shouldEnableButton: true)
-        game.resetGameState()
-        gameTitleLabel.text = resetGameTitleName
     }
 
     func resetAllButtons(shouldResetButtonColor resetColor: Bool, shouldEnableButton: Bool) {
@@ -129,29 +122,46 @@ class GameBoardController: UIViewController {
         // set the title to a blank string and state to normal
         let title = "\(row),\(col)"
         button.setTitle(title, forState: .Normal)
+        button.setTitleColor(.clearColor(), forState: .Normal)
         // attach an action function call "handleButtonTapped"
         button.addTarget(self, action: #selector(handleButtonTapped), forControlEvents: .TouchUpInside)
         // once we finished constructing the button we add the button to the view
         containerView.addSubview(button)
     }
 
+
+    // MARK: - Button Handler
+    func handleResetButton(resetButton: UIButton) {
+        // reset all the colors... let's make a function that reset all the button colors to blue except the resetbutton itself
+        isPlayerOne = true
+        resetAllButtons(shouldResetButtonColor: true, shouldEnableButton: true)
+        game.resetGameState()
+        gameTitleLabel.text = resetGameTitleName
+    }
+
     func handleButtonTapped(button: UIButton) {
         // we want to disable the button once it's tapped... prevent cheaters
         button.enabled = false
-        // isPlayerOne is always true for initial state... once button is tapped the boolean value of isPlayerOne will alternate.
+
+        // grab the title of the button to determine the position of the button (tuple value)
         guard let title = button.currentTitle else { return }
+        // convert string to tuple
         let indexValue = game.convertStringToTupleFrom(title)
+
+        // isPlayerOne is always true for initial state... once button is tapped the boolean value of isPlayerOne will alternate.
         if isPlayerOne {
+            // b4 we check for winner.... we need to set the player value in the ticTacToeState
             game.setPlayerMarkOn(indexValue, forPlayer: .PlayerOne)
             button.backgroundColor = .redColor()
             isPlayerOne = false
         } else {
+            // b4 we check for winner.... we need to set the player value in the ticTacToeState
             game.setPlayerMarkOn(indexValue, forPlayer: .PlayerTwo)
             button.backgroundColor = .greenColor()
             isPlayerOne = true
         }
-        // b4 we check for winner.... we need to set the player value in the ticTacToeState
 
+        // if there's a winner grab the player's Name and display it on gameTitleLabel and reset all the button color and state
         if let playerName = game.checkForWinner() {
             gameTitleLabel.text = playerName
             resetAllButtons(shouldResetButtonColor: false, shouldEnableButton: false)
